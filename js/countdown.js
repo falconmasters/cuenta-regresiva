918cdafe127af980775b325366f7625fab441c57
Carlos Arturo <carlos@falconmasters.com>
2020-02-18 13:34:54 -0600

    Agregando archivos finales

---
+++ js/countdown.js
@@ -0,0 +1,30 @@
+simplyCountdown('#cuenta', {
+	year: 2025, // required
+	month: 2, // required
+	day: 19, // required
+	hours: 7, // Default is 0 [0-23] integer
+	minutes: 31, // Default is 0 [0-59] integer
+	seconds: 0, // Default is 0 [0-59] integer
+	words: { //words displayed into the countdown
+		days: 'Día',
+		hours: 'Hora',
+		minutes: 'Minuto',
+		seconds: 'Segundo',
+		pluralLetter: 's'
+	},
+	plural: true, //use plurals
+	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+	// in case of inline set to false
+	enableUtc: true, //Use UTC as default
+	onEnd: function() {
+		document.getElementById('portada').classList.add('oculta');
+		return; 
+	}, //Callback on countdown end, put your own function here
+	refresh: 1000, // default refresh every 1s
+	sectionClass: 'simply-section', //section css class
+	amountClass: 'simply-amount', // amount css class
+	wordClass: 'simply-word', // word css class
+	zeroPad: false,
+	countUp: false
+});
